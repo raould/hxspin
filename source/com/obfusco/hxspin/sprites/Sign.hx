146121c21d358a3b88b412210a4cc1b7e2c270ce
Raoul Duke <raould@gmail.com>
2017-01-14 23:50:17 -0800

    checkpoint

--- source/com/obfusco/hxspin/sprites/Sign.hx
+++ source/com/obfusco/hxspin/sprites/Sign.hx
@@ -5,6 +5,10 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class Sign extends FlxSprite
 {
+	private static inline var MaxRotDegrees = 30;
+	private static inline var MinRotDegrees = -30;
+	private var degreesPerSecond = 100;
+
 	public function new(?X:Float=0, ?Y:Float=0)
 	{
 		super(X, Y);
@@ -14,6 +18,16 @@ class Sign extends FlxSprite
 
 	override public function update(elapsed:Float):Void
 	{
-		angle += 100 * elapsed;
+		// todo: software rotation is purportedly 10x slower than rotation via tilesheet frames.
+		// todo: animate the player at least a little and match them up.
+		angle += degreesPerSecond * elapsed;
+		if( degreesPerSecond > 0 && angle > MaxRotDegrees )
+		{
+			degreesPerSecond *= -1;
+		}
+		else if( degreesPerSecond < 0 && angle < MinRotDegrees )
+		{
+			degreesPerSecond *= -1;			
+		}
 	}
 }
