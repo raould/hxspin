f7efadd13017abca9ffec68594607c376e99f6e6
Raoul Duke <raould@gmail.com>
2017-01-20 22:48:20 -0800

    checkpoint

--- source/com/obfusco/hxspin/Tracker.hx
+++ source/com/obfusco/hxspin/Tracker.hx
@@ -17,8 +17,7 @@ class Tracker {
 		this.filename = filename;
 		this.bpm = bpm;
 		this.sound = FlxG.sound.load( filename, 1 );
-        // bpm = x beats per 60 seconds.
-        // therefore 1 beat -> x/x : 60/x
+        // bpm = x beats per 60 seconds, so 1 beat = x/x = 60/x.
 		this.msecPerBeat = (60/bpm) * 1000;
 		this.lastBeatMsec = 0;
 	}
@@ -29,19 +28,18 @@ class Tracker {
 
 	public function stop():Void {
 		sound.stop();
-		// todo: is this the right kind of resetting?
+		// todo: uh, is this the right kind of resetting?
 		this.lastBeatMsec = 0;
 	}
 
 	public function didBeatHappen():Bool {
 		var dt = sound.time - lastBeatMsec;
 		var did = dt >= msecPerBeat;
-		// skip beats if we're not polled fast enough.
+		// yes, this skip beats if we're not polled fast enough.
 		trace( sound.time, lastBeatMsec, dt, msecPerBeat, did );
 		if( did ) {
 		  lastBeatMsec = sound.time;
 		}
-		
 		return did;
 	}
 }
